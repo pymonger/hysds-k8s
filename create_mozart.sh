24ed69f6bd82ffba969854f703cafa0d20df22ec
Gerald Manipon <geraldjohn.m.manipon@jpl.nasa.gov>
2018-10-07 11:11:49 -0700

    provide url for kubernetes on mac

--- create_mozart.sh
+++ create_mozart.sh
@@ -25,13 +25,19 @@ MOZART_FQDN=$(kubectl get pod -l run=mozart -o jsonpath="{.items[0].spec.nodeNam
 MOZART_IP=$(kubectl get pod -l run=mozart -o jsonpath="{.items[0].status.hostIP}")
 MOZART_PORT=$(kubectl get service mozart -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 MOZART_FLOWER_PORT=$(kubectl get service mozart -o jsonpath='{.spec.ports[?(@.name=="flower")].nodePort}')
+echo "The mozart interfaces will be accessible by one the urls below depending on how your Kubernetes cluster is setup:"
 echo "Figaro:          https://${MOZART_FQDN}:${MOZART_PORT}/figaro"
 echo "                 https://${MOZART_IP}:${MOZART_PORT}/figaro"
+echo "                 https://localhost:${MOZART_PORT}/figaro"
 echo "Mozart REST API: https://${MOZART_FQDN}:${MOZART_PORT}/mozart/api/v0.1/"
 echo "                 https://${MOZART_IP}:${MOZART_PORT}/mozart/api/v0.1/"
+echo "                 https://localhost:${MOZART_PORT}/mozart/api/v0.1/"
 echo "Mozart ES:       http://${MOZART_ELASTICSEARCH_FQDN}:${MOZART_ELASTICSEARCH_PORT}/_plugin/head"
 echo "                 http://${MOZART_ELASTICSEARCH_IP}:${MOZART_ELASTICSEARCH_PORT}/_plugin/head"
+echo "                 http://localhost:${MOZART_ELASTICSEARCH_PORT}/_plugin/head"
 echo "Flower:          http://${MOZART_FQDN}:${MOZART_FLOWER_PORT}"
 echo "                 http://${MOZART_IP}:${MOZART_FLOWER_PORT}"
+echo "                 http://localhost:${MOZART_FLOWER_PORT}"
 echo "RabbitMQ Admin:  http://${MOZART_RABBITMQ_FQDN}:${MOZART_RABBITMQ_PORT}"
 echo "                 http://${MOZART_RABBITMQ_IP}:${MOZART_RABBITMQ_PORT}"
+echo "                 http://localhost:${MOZART_RABBITMQ_PORT}"
