735f8cb442cfe981e4a2dfef7a303a15db566477
Gerald Manipon <pymonger@gmail.com>
2018-10-12 16:38:51 -0400

    save

--- create_grq.sh
+++ create_grq.sh
@@ -2,7 +2,13 @@
 BASE_PATH=$(dirname "${BASH_SOURCE}")
 BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
-# create configmap
+# create global configmap
+kubectl get configmap hysds-global-config
+if [ $? -ne 0 ]; then
+  kubectl create configmap hysds-global-config --from-file=${BASE_PATH}/config
+fi
+
+# create grq configmap
 kubectl create configmap hysds-grq-config --from-file=${BASE_PATH}/grq/config
 
 # deploy grq services
