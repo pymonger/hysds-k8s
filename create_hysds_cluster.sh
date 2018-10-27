c5e67fd0f54530ad156d0dbcdaddac891dcef77c
Gerald Manipon <pymonger@gmail.com>
2018-10-27 19:01:05 +0000

    update credentials and paths

--- create_hysds_cluster.sh
+++ create_hysds_cluster.sh
@@ -4,6 +4,22 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 source ${BASE_PATH}/utils.sh
 
 
+# get access secret keys
+if [ "$#" -ne 2 ]; then
+  echo "Enter access and secret keys as args: $0 <access key> <secret key>"
+  echo "e.g.: $0 12345 54321"
+  exit 1
+fi
+ACCESS_KEY=$1
+SECRET_KEY=$2
+
+
+cat ${BASE_PATH}/config/aws_credentials.tmpl | \
+  sed "s#__ACCESS_KEY__#${ACCESS_KEY}#" | \
+  sed "s#__SECRET_KEY__#${SECRET_KEY}#" > \
+  ${BASE_PATH}/config/aws_credentials
+
+
 # deploy minio services
 #${BASE_PATH}/create_minio.sh
 
